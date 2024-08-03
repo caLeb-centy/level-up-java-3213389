5595af4279c61147bd6d11d65b6bb5a95dd98ff9
caLeb-centy <167354117+caLeb-centy@users.noreply.github.com>
2024-08-03 06:18:31 +0000

    isPasswordComplex completed

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,11 +1,41 @@
 package com.linkedin.javacodechallenges;
 
 import java.util.Scanner;
-
+//import java.lang.*;
 public class App {
 
     public static boolean isPasswordComplex(String password) {
-        return false;
+        /*
+        must have 6 characters
+        must have one upper case character
+        musthave one lowercase character
+        must have one number
+        */
+        boolean testOne, testTwo, testThree;
+        boolean status = false;
+        int n = password.length();
+        testOne = testTwo = testThree = false;
+        int i =0;       
+        if(n >= 6){
+            while(i < n && status != true){
+                if(testOne == false && Character.isDigit(password.charAt(i)) == true){
+                    testOne = true;
+                }
+                else if(testTwo == false && Character.isUpperCase(password.charAt(i)) == true){
+                    testTwo = true;
+                }
+                else if (testThree == false && Character.isLowerCase(password.charAt(i)) == true) {
+                    testThree = true;
+                }
+                
+                if (testOne == true && testTwo == true && testThree == true) {
+                    status = true;
+                }
+                ++i;
+            }            
+        }
+
+        return status;
     }
 
     public static void main(String[] args) {
