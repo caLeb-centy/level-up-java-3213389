7553f9c6c9160ec0a8e6552bfc373f3baaa45840
caLeb-centy <167354117+caLeb-centy@users.noreply.github.com>
2024-08-04 10:22:37 +0000

    person completed, very simplistic

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
@@ -1,3 +1,36 @@
+package com.linkedin.javacodechallenges;
+
 public class Person {
-  
+  private int age;
+  private String firstName = new String();
+  private String lastName = new String();
+
+  protected String getfirstName(){
+    return firstName;
+  }
+
+  protected String getlastName() {
+    return lastName;
+  }
+
+  protected int getAge(){
+    return age;
+  }
+
+  protected void setlastName(String newLastName) {
+    this.lastName = newLastName;
+  }
+
+  protected void setfirstName(String newfirstName) {
+    this.firstName = newfirstName;
+  }
+
+  protected void setAge(int newAge) {
+    this.age= newAge;
+  }
+
+  protected void introduction(){
+    System.out.println("hello, my name is "+firstName + "" + lastName + ".");
+    System.out.println("I am "+ age +" years old");
+  }
 }
--- javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
+++ javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
@@ -1,5 +1,6 @@
 package com.linkedin.javacodechallenges;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -17,4 +18,22 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+
+    @Test
+    public void name() {
+        Person test = new Person();
+        test.setfirstName("John");
+        test.setlastName("Doe");
+        assertEquals("John", test.getfirstName());
+        assertEquals("Doe", test.getlastName());
+    }
+
+    @Test
+    public void age() {
+        Person test = new Person();
+        test.setAge(25);
+        assertEquals(25, test.getAge());
+    }
+
 }
