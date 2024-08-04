09a66d3eb42ae8896314d5d59baf805b5a641ef4
caLeb-centy <167354117+caLeb-centy@users.noreply.github.com>
2024-08-04 10:03:26 +0000

    calculateWaterBill completed

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -3,10 +3,22 @@ package com.linkedin.javacodechallenges;
 import java.util.Scanner;
 
 public class App {
-
     public static double calculateWaterBill(double gallonsUsage) {
         // TODO: Implement method
-        return 0;
+        /*
+        minimum $18.84 for 1496 gallons (two ccf)
+        one ccf is 748
+        usee more than 2 ccfs, charges $3.90 per additional ccr
+        use 1800 gallons of warer, charges $18.84 pluus $3.90
+        for the extra volume
+        */
+        double ccfUsage  = gallonsToCCF(gallonsUsage);
+        double moolah = 18.84;
+        if(ccfUsage > 2){
+            moolah = moolah + (Math.ceil(ccfUsage-2) * 3.90);
+            moolah = Math.round(moolah * 100.0) / 100.0;//round to two decimal places
+        }
+        return moolah;
     }
 
     public static void main(String[] args) {
@@ -18,4 +30,14 @@ public class App {
                 calculateWaterBill(usage));
         scanner.close();
     }
+
+    private static double gallonsToCCF(double gallons) {
+        double ccf = 748;
+        double gallonsConverted = gallons / ccf;
+        return gallonsConverted;
+    }
+
 }
+
+
+ 
