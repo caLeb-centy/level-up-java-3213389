d44bee57f502018f22d6cd5cc2dba13d6e13b9e4
caLeb-centy <167354117+caLeb-centy@users.noreply.github.com>
2024-08-06 04:37:56 +0000

    hundredDAys completed barebones

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,21 @@
 package com.linkedin.javacodechallenges;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 public class App 
 {
     // Create function to calculate the date that's 
     // 100 days from now
 
+    protected static String hundredDays(){
+        //create a default time zone 
+        LocalDate currentDate = LocalDate.now(ZoneId.of("America/Chicago"));
+        return currentDate.plusDays(100).toString();
+    }
+
     public static void main( String[] args )
     {
-        System.out.println("100 days from now is... " 
-            /* add function call */);
+        System.out.println("100 days from now is... "   
+            + hundredDays());
     }
 }
