5d1a061e67a0adb68113efd7de92586ce8ff262f
caLeb-centy <167354117+caLeb-centy@users.noreply.github.com>
2024-08-09 10:06:51 +0000

    03_01b completed

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/TeamUtils.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/TeamUtils.java
@@ -1,7 +1,9 @@
 package com.linkedin.javacodechallenges;
 
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Random;
+
 
 public class TeamUtils {
 
@@ -16,6 +18,17 @@ public class TeamUtils {
   }
 
   public static void revealResults(List<Team> teams) {
+    /*
+     * Game has a series of rounds
+     * Each player and partner get points
+     * write a program that automatically determines the winner
+     * The program should also print the final score
+     */
 
+    PriorityQueue<Team> pq = new PriorityQueue<>( (teamOne, teamTwo) 
+      -> (Integer.compare(teamOne.sumTotalScore(), teamTwo.sumTotalScore())) );
+    Team winner = pq.poll();
+    System.out.println("The winning team is " + winner + 
+      " with a total of " + winner.toString() + " points.");
   }
 }
