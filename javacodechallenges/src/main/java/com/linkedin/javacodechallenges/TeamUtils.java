f2cf3d249cbaa118648cfa76169243a750d4da0e
caLeb-centy <167354117+caLeb-centy@users.noreply.github.com>
2024-08-09 10:57:08 +0000

    worked on 03_01b

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/TeamUtils.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/TeamUtils.java
@@ -26,9 +26,33 @@ public class TeamUtils {
      */
 
     PriorityQueue<Team> pq = new PriorityQueue<>( (teamOne, teamTwo) 
-      -> (Integer.compare(teamOne.sumTotalScore(), teamTwo.sumTotalScore())) );
-    Team winner = pq.poll();
-    System.out.println("The winning team is " + winner + 
-      " with a total of " + winner.toString() + " points.");
+      -> (Integer.compare(teamTwo.sumTotalScore(), teamOne.sumTotalScore())) );
+    pq.addAll(teams);
+
+    printResults(pq);
+
+   // Team winner = pq.poll();
+ //   System.out.println("The winning team is " + winner + 
+  //    " with a total of " + winner.toString() + " points.");
+  }
+
+  static protected void printResults(PriorityQueue<Team> pq){
+    Team winner = pq.poll(); //created just to isolate winner 
+    System.out.println("Now for the results, the WINNER is...\n");
+    System.out.println("With " + winner.sumTotalScore() + " points, it's team "
+        + printPlayers(winner) + "!\n");
+    System.out.println("\n");
+
+    while(!pq.isEmpty()){
+      System.out.println("Then we have... \n");
+      System.out.println("With " + pq.peek().sumTotalScore() + " points, it's team "
+        + printPlayers(pq.poll()) + "!\n");
+      System.out.println("\n");
+    }
+  }
+
+  static protected String printPlayers(Team team){
+    String players = (team.getPlayer1() + " and "+ team.getPlayer2());
+    return players;
   }
 }
